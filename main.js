8beb0d578b8f8194f987ad28eb4c2a16db051165
Ujef55 <ujef.kelawala123@gmail.com>
2023-07-12 01:35:42 +0530

    fixing javascript

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 const search = document.querySelector('#search');
+const box = document.querySelector('.box');
 
 
 const searchFilter = () => {
     let filter = search.value.toUpperCase();
     let li = document.querySelectorAll('.listItemsName');
+    let head = document.querySelectorAll('.head'); 
 
     for (let i = 0; i < li.length; i++){
-        console.log(li[i].textContent);
+        let item = li[i];
+        
+        let itemName = item.textContent.toUpperCase();
+
+            if (itemName.includes(filter)) {
+                item.style.display = 'block';
+            } else {
+                item.style.display = 'none';
+            }
     }
   
     
